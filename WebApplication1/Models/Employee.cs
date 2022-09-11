e0feb7239352988dc0a4da27ebb0f14a24cfd8d5
cjones1047 <cjmj1047@gmail.com>
2022-09-11 08:10:12 -0500

    Employee.cs created, models for Department and Employee created, build successful

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Deparments")]
+    public class Department
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string? DeptName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        
+        public int Id { get; set; }
+        [ForeignKey("Department")]
+
+        public int FKDeptId { get; set; }
+
+        public string? EmpName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Department? Department { get; set; }
+
+    }
+}
